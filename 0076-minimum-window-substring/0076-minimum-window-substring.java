9f87b6b5e50d9cad7e10f0018b81251d32774ace
prakant_sharma <166603325+prakantsharma@users.noreply.github.com>
2026-06-30 11:23:30 +0530

    Time: 32 ms (20.53%), Space: 47.9 MB (19.61%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,58 @@
+import java.util.HashMap;
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) {
+            return "";
+        }
+
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        // Store frequency of characters in t
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int have = 0;
+        int needCount = need.size();
+
+        int left = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+
+            window.put(ch, window.getOrDefault(ch, 0) + 1);
+
+            if (need.containsKey(ch) &&
+                window.get(ch).intValue() == need.get(ch).intValue()) {
+                have++;
+            }
+
+            while (have == needCount) {
+                // Update minimum window
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                window.put(leftChar, window.get(leftChar) - 1);
+
+                if (need.containsKey(leftChar) &&
+                    window.get(leftChar) < need.get(leftChar)) {
+                    have--;
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+}
