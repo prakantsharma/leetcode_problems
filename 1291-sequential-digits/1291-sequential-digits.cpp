f36a21ff183020cfdf6c0bf8e7adc7309fb7cf40
prakant_sharma <166603325+prakantsharma@users.noreply.github.com>
2026-07-13 08:51:14 +0530

    Time: 0 ms (100%), Space: 8.3 MB (76.91%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string s = "123456789";
+        vector<int> ans;
+
+        int l = to_string(low).size();
+        int r = to_string(high).size();
+
+        for (int len = l; len <= r; len++) {
+            for (int i = 0; i + len <= 9; i++) {
+                int num = stoi(s.substr(i, len));
+                if (num >= low && num <= high)
+                    ans.push_back(num);
+            }
+        }
+
+        return ans;
+    }
+};
